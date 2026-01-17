3168c19e933e443ec7bb3fa5ec0b58d370e87c33
Ana Budai <anamaria_b2006@yahoo.com>
2026-01-17 20:45:22 +0200

    AI Agents: improve promting engineering

--- using_OpenAI_module.py
+++ using_OpenAI_module.py
@@ -1,6 +1,6 @@
 from openai import OpenAI
 from dotenv import load_dotenv
-import os
+import os, json
 
 load_dotenv()
 OPEN_AI_KEY = os.getenv("AUTH_KEY_TOKEN")
@@ -8,6 +8,16 @@ OPEN_AI_KEY = os.getenv("AUTH_KEY_TOKEN")
 client_open_ai = OpenAI(api_key = OPEN_AI_KEY,);
 
 def generate_x_post(input):
+    with open("post-generated.json", "r") as post_generated_file:
+        examples = json.load(post_generated_file)
+    examples_all = ""
+    for i, example in enumerate(examples):
+        examples_all += f"""
+        <example-{i}>
+            <topic>{example["topic"]}</topic>
+            <generated-post>{example["post"]}</generated-post>
+        </example-{i}>
+        """
     prompt = f'''
                 You are an expert social media manager and you excel at crafting viral and highly engaging posts social media
 
@@ -17,7 +27,13 @@ def generate_x_post(input):
 
                 Here's the topic provided by the user for which you need to generate the post:   
                 <topic>{input}</topic>
-'''  
+
+                Here are some examples of topics and generated posts:
+                <examples>
+                    {examples_all}
+                </examples>
+                Please use the tone, language, information, content from the examples above to generate user responses
+    '''  
     response = client_open_ai.responses.create(
         model = "gpt-4o",
         input = prompt
